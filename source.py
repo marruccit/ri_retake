6fe7b427dc9df4964764a52c2e9cb6c476e5428a
Tom Marrucci <tom.marrucci@gmail.com>
2018-05-23 17:10:55 +0200

    plein de changement

--- source.py
+++ source.py
@@ -4,6 +4,8 @@ import math
 import operator
 import csv
 import scipy
+import pandas as pd
+from scipy import spatial
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import brown
 from stop_words import get_stop_words
@@ -12,7 +14,6 @@ from nltk.stem.snowball import FrenchStemmer
 
 
 path_doc = "documents"
-
 # Mot qui ne sont pas important
 stop_words = get_stop_words('fr')
 # Ajout de la ponctuation
@@ -38,11 +39,6 @@ def tokenize(documents):
         documents_tok[key] = words_tmp
     return(documents_tok)
 
-def tokenize_sentence(sentence):
-    words_tmp = nltk.word_tokenize(sentence)
-    words_tmp = [w.lower() for w in words_tmp] # lowercase
-    words_tmp = [word for word in words_tmp if word not in stop_words] # remove stop_words
-    return(words_tmp)
 
 
 # Comptage des mots dans les différents documents : Term Frequency
@@ -54,7 +50,9 @@ def computeFrequency(documents_tok):
 
 
 # Calcul du IDF des mots
-def computeIDF(documents_tok, nb_doc):
+def computeIDF(documents_tok, nb_doc, smooth=0):
+    if (smooth > 1):
+        smooth = 1
     idf = {}
     for word in set_words:
         #tfidf_tot[word] = 0
@@ -62,7 +60,7 @@ def computeIDF(documents_tok, nb_doc):
         for key, value in documents_tok.items():
             if word in value:
                 occur_doc += 1
-        idf[word] = math.log10((len(documents) / occur_doc))
+        idf[word] = math.log10(smooth + (len(documents) / occur_doc))
     return(idf)
 
 
@@ -89,11 +87,13 @@ def sumTFIDF(tfidf):
 
 def save(sorted_tfidf, path='saveTFIDF_tot2.csv'):
     print("Sauvegarde du TFIDF")
-    with open(path, 'w',  encoding='utf-8') as csv_file:
-        writer = csv.writer(csv_file)
+    with open(path, 'w',  encoding='utf-16') as csv_file:
+        writer = csv.writer(csv_file, delimiter=';')
+        writer.writerow(['word', 'value'])
         for pair in sorted_tfidf:
            writer.writerow([pair[0], pair[1]])
 
+# OLD OLD OLD
 def create_cooccurrence_matrix(filename,window_size):
     vocabulary={}
     data=[]
@@ -115,7 +115,102 @@ def create_cooccurrence_matrix(filename,window_size):
                 data.append(1.); row.append(i); col.append(j);
     cooccurrence_matrix=scipy.sparse.coo_matrix((data,(row,col)))
     return vocabulary,cooccurrence_matrix
+
+
+
+# Tokenize a sentence
+def tokenize_sentence(sentence):
+    words_tmp = nltk.word_tokenize(sentence)
+    words_tmp = [w.lower() for w in words_tmp] # lowercase
+    words_tmp = [word for word in words_tmp if word not in stop_words] # remove stop_words
+    return(words_tmp)
+
+# Créer la matrice de cooccurence sur le texte
+def getCooccurenceMatrix(documents, window_size):
+    vocabulary={}
+    data=[]
+    row=[]
+    col=[]
+    content_docs = '.\n'.join(documents.values())
+    content_docs = content_docs.replace("\n", "")
+    for sentence in content_docs.split("."):
+        sentence=sentence.strip()
+        tokens=[token for token in tokenize_sentence(sentence) if token!=u""]
+        for pos,token in enumerate(tokens):
+            i=vocabulary.setdefault(token, len(vocabulary))
+            start=max(0,pos-window_size)
+            end=min(len(tokens),pos+window_size+1)
+            for pos2 in range(start,end):
+                if pos2==pos: 
+                    continue
+                j=vocabulary.setdefault(tokens[pos2], len(vocabulary))
+                data.append(1.); row.append(i); col.append(j);
+    cooccurrence_matrix=scipy.sparse.coo_matrix((data,(row,col)))
+    return vocabulary,cooccurrence_matrix
+
+# Read the save file and return the 10% last words
+def getKeepWords(filename, percent_selected=0.1):
+    words = []
+    with open(filename, 'r',  encoding='utf-16') as csv_file:
+       reader = csv.reader(csv_file, delimiter=';')
+       for row in reader:
+           words.append(row[0])
+    nSelect = int(len(words) * percent_selected)
+    return words[-nSelect:]
+
+
+# Retourne la liste des index à garder
+def getListIndex(words, vocabulary):
+    indexes = []
+    for word in words:
+        indexes.append(vocabulary.get(word))
+    return indexes
+
+# Peut etre pas utile
+def getReduceMatrix(keep_index, inverse_vocabulary, matrix):
+    new_voc = {}
+    new_matrix = []
+    i = 0
+    matrix = matrix.toarray()
+    for pos,row in enumerate(matrix):
+        if pos in keep_index:
+            new_matrix.append(row)
+            new_voc[i] = 0
+            i += 1
+
+
+
 '''
+
+def getReduceMatrix(list, vocabulary, matrix):
+    new_voc = {}
+inv_map = {v: k for k, v in my_map.items()}
+
+
+
+def getCooccurenceMatrix(documents, words, window_size):
+    vocabulary={}
+    content_docs = '.\n'.join(documents.values())
+    content_docs = content_docs.replace("\n", "")
+    for sentence in content_docs.split("."):
+        sentence=sentence.strip()
+        tokens=[token for token in tokenize_sentence(sentence) if token!=u""]
+        for word in words:
+            if word in tokens:
+
+
+def computeCosine(voc, coo_matrix):
+    arr = coo_matrix.toarray()
+    eqs = []
+    asso = []
+    for pos, line in enumerate(a):
+        cos = 1 - spatial.distance.cosine(array[0], line)
+        if cos > 0.9:
+            eqs.append()
+        elif cos > 0.7:
+            return
+
+
 
 # Comptage des mots de manière globale (compilation de tous les dictionnaires précédents) : peut etre pas utile
 for value in documents_count.values():
@@ -144,10 +239,12 @@ if __name__ == "__main__":
     documents_tok = {} 
     set_words = set()
     idf = {}
+    idf_smooth = {}
     tfidf = {}
     tfidf_tot = {}
     print("Chargement du corpus")
     documents = loadCorpus()
+    
     nb_doc = len(documents)
     print("Tokenization du corpus")
     documents_tok = tokenize(documents)
@@ -156,11 +253,24 @@ if __name__ == "__main__":
     documents_count = computeFrequency(documents_tok)
     
     print("Calcul de IDF des mots " + "("+ str(len(set_words)) + " mots)")
-    idf = computeIDF(documents_tok, nb_doc)
+    idf = computeIDF(documents_tok, nb_doc)    
     print("Calcul du TFIDF des mots")
     tfidf = computeTFIDF(documents_count, idf)
     print("Aggrégation du TFIDF des mots")
     tfidf_tot = sumTFIDF(tfidf)
     print("Tri du TFIDF")
     sorted_tfidf = sorted(tfidf_tot.items(), key=operator.itemgetter(1))
+    save(sorted_tfidf, 'tfidf_tot.csv')
+
+    print("Calcul de IDF smooth des mots " + "("+ str(len(set_words)) + " mots)")
+    idf_smooth = computeIDF(documents_tok, nb_doc, 1)
+    print("Calcul du TFIDF des mots")
+    tfidf_smooth = computeTFIDF(documents_count, idf_smooth)
+    print("Aggrégation du TFIDF des mots")
+    tfidf_tot_smooth = sumTFIDF(tfidf_smooth)
+    print("Tri du TFIDF")
+    sorted_tfidf_smooth = sorted(tfidf_tot_smooth.items(), key=operator.itemgetter(1))
+    save(sorted_tfidf_smooth, 'tfidf_tot_smooth.csv')
+
+    words = getWordsThesaurus(sorted_tfidf)
 '''
